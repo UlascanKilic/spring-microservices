483e947dea8cd605b56c93af7453f5cc8fac1f76
S. Ulaşcan Kılıç <ulascank1@gmail.com>
2023-05-18 13:15:13 +0300

    project structure fixed

--- microservice-parent/order-service/src/main/java/com/ulascan/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ulascan.orderservice.repository;
-
-import com.ulascan.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
