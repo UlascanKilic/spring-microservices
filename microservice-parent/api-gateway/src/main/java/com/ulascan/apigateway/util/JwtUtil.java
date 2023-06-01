5fc02251dfcf9919cebacb28b041b23fa1833a85
S. Ulaşcan Kılıç <ulascank1@gmail.com>
2023-06-02 01:32:01 +0300

    added env files

--- microservice-parent/api-gateway/src/main/java/com/ulascan/apigateway/util/JwtUtil.java
+++ microservice-parent/api-gateway/src/main/java/com/ulascan/apigateway/util/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -12,8 +13,8 @@ import java.security.Key;
 public class JwtUtil {
 
 
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
-
+    @Value("${application.security.jwt.secret-key}")
+    public static String SECRET;
 
     /**
      * Validates the given JWT token.
